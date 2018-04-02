1570552d835d3f433475b718f09d876fd85b1ceb
pdgwelle <pdgwelle@gmail.com>
2018-04-02 14:39:36 -0400

    Make generate_data executable from the command line

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,5 @@
+import sys
+
 import pysolver as ps
 import pickle
 
@@ -7,3 +9,9 @@ def generate_dataset(n_games=1000, n_moves=15):
 	with open("models/" + str(n_games) + ".pkl", "wb") as f:
 		pickle.dump(model, f)
 	return model
+
+if __name__ == '__main__':
+	n_games = int(sys.argv[1])
+	n_moves = int(sys.argv[2])
+
+	model = generate_dataset(n_games=n_games, n_moves=n_moves)
